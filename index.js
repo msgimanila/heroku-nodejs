79f9b9738d9e63e845f623ace27c008c0ad48347
Mario Garcia <msgimanila@yahoo.com>
2017-04-16 18:13:09 +0800

    1

--- index.js
+++ index.js
@@ -15,7 +15,23 @@ app.get('/', function(request, response) {
 });
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port...', app.get('port'));
+  console.log('Node app is running on port....', app.get('port'));
 });
+var MongoClient = require('mongodb').MongoClient
+
+MongoClient.connect('mongodb://mongouser:mongo501@ds149820.mlab.com:49820/mongomanila', function (err, db) {
+  if (err) throw err
+
+  db.collection('mongomanila').find().toArray(function (err, result) {
+    if (err) throw err
+     app.get('/d/', function (req, res) {
+	   
+     res.json(result[1]);
+  
+})
+    //console.log(result)
+	 
+  })
+})
 
 
